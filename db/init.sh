57e6b73e461021b6ae35700441962dd8cbf100bc
wreulicke <masaya2dies@gmail.com>
2019-10-16 16:51:46 +0900

    add index

--- db/init.sh
+++ db/init.sh
@@ -18,3 +18,5 @@ fi
 mysql -uisucon torb -e 'ALTER TABLE reservations DROP KEY event_id_and_sheet_id_idx'
 gzip -dc "$DB_DIR/isucon8q-initial-dataset.sql.gz" | mysql -uisucon torb
 mysql -uisucon torb -e 'ALTER TABLE reservations ADD KEY event_id_and_sheet_id_idx (event_id, sheet_id)'
+mysql -uisucon torb -e 'ALTER TABLE reservations ADD INDEX reservations_event_id_canceled_at(`event_id`, `canceled_at`)'
+mysql -uisucon torb -e 'ALTER TABLE events ADD INDEX events_id_public_fg(`id`, `public_fg`)'
